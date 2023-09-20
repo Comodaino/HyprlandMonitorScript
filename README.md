f0f5e0da1af6572698ce8cf2b8c996e1a7762e04
Comodaino <alespineto@gmail.com>
2023-09-20 14:44:30 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HyprlandMonitorScript
-Little and stupid script to switch monitor modes on a dual monitor setup on hyprland
+## Little and stupid script to switch monitor modes on a dual monitor setup on hyprland
+To use it move left, right, mirrored and monitor inside the hyprland config folder
--- monitorscript.sh
+++ monitorscript.sh
@@ -1,15 +1,12 @@
+case $(cat ~/.config/hypr/monitor.conf | grep '#' | cut -b 2) in
+  r) cp ~/.config/hypr/left ~/.config/hypr/monitor.conf;
+    export MONITOR_MODE='l';;
+  m) cp ~/.config/hypr/right ~/.config/hypr/monitor.conf;
+    export MONITOR_MODE="r";;
+  l) cp ~/.config/hypr/mirrored ~/.config/hypr/monitor.conf;
+    export MONITOR_MODE="m";;
+esac
+
+export MONITOR_MODE;
 
 
-
-
-rm ~/.config/hypr/monitor.conf
-
-
-while getopts l:r:m: flag
-do
-    case "${flag}" in
-        l) cp ~/.config/hypr/left ~/.config/hypr/monitor.conf;;
-        r) cp ~/.config/hypr/right ~/.config/hypr/monitor.conf;; 
-        m) cp ~/.config/hypr/mirrored ~/.config/hypr/monitor.conf;;
-    esac
-done
